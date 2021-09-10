446ad94a4e9c0b389b5e222a88795c394e680f6a
Nandkishor J <snape0407@gmail.com>
2021-09-10 16:49:27 +0530

    First coding lesson, baby steps

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {  
+    cout << "I am" << endl << "very" << endl << endl;
+    cout << "smart" << endl;
+    cout << "tm" << endl << "tm tm" << endl << endl;
+    cout <<"tm tm tm" << endl << endl << endl;
+    cout << "yes";
+    return 0;
+}
