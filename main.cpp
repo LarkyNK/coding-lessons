8cfa32cf28affcc92e2bc8e9ec86f1e418ec64e3
Nandkishor J <snape0407@gmail.com>
2021-09-14 20:42:18 +0530

    Operations 1

--- main.cpp
+++ main.cpp
@@ -2,13 +2,10 @@
 using namespace std;
 
 int main() {  
-    int age1 = 21;
-    float height1 = 5.10F;
-    int age2 = 69;
-    float height2 = 6.2F;
-    cout << "Person 1 age:" << age1 << "\n";
-    cout << "Person 1 height:" << height1 << "\n";
-    cout << "Person 2 age:" << age2 << "\n";
-    cout << "Person 2 height:" << height2 << "\n";
+    int a= 4;
+    long b= 6;
+    double c= 8;
+    double d= a*c - a*b;
+    cout << d << "\n";
     return 0;
 }
