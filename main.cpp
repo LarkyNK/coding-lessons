befaaaa727d081fc29ec014dc6da06de2f56a8c8
Nandkishor J <snape0407@gmail.com>
2021-09-14 18:24:15 +0530

    Variables

--- main.cpp
+++ main.cpp
@@ -2,8 +2,13 @@
 using namespace std;
 
 int main() {  
-    cout << "name \t Description \t Phone number \t\t Address\n";
-    cout << "Name Description Phone number Address";
-    cout << "\\";
+    int age1 = 21;
+    float height1 = 5.10F;
+    int age2 = 69;
+    float height2 = 6.2F;
+    cout << "Person 1 age:" << age1 << "\n";
+    cout << "Person 1 height:" << height1 << "\n";
+    cout << "Person 2 age:" << age2 << "\n";
+    cout << "Person 2 height:" << height2 << "\n";
     return 0;
 }
