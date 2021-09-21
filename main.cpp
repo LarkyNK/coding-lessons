5cd2eab4c517814b9651506b968691932db12e78
Nandkishor J <snape0407@gmail.com>
2021-09-21 20:54:12 +0530

    Discriminant Activity

--- main.cpp
+++ main.cpp
@@ -1,16 +1,30 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int x; int y;  
-    cout << "Enter value of x\n";
-    cin >> x;
-    cout << "Enter value of y\n";
-    cin >> y;
-    if (x != y) {
-    cout << "31 and 31 are equal";    
-    } else {
-    cout << "31 and 31 aren't equal";
+int main()
+{
+    double a;
+    double b;
+    double c;
+    cout << "Enter a\n";
+    cin >> a;
+    cout << "Enter b\n";
+    cin >> b;
+    cout << "Enter c\n";
+    cin >> c;
+    double d = (b * b) - 4 * a * c;
+    cout << "Discriminant: " << d << "\n";
+    if (d > 0)
+    {
+        cout << "Discriminant is positive, hence equation has 2 real and different roots\n";
+    }
+    else if (d == 0)
+    {
+        cout << "Discriminant is equal to zero, hence equation has 2 real but equal roots\n";
+    }
+    else if (d < 0)
+    {
+        cout << "Discriminant is less than zero, hence equation has imaginary roots\n";
     }
     return 0;
 }
