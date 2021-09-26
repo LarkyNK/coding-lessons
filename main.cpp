7e6ba9be716808b559f3988396c2eb29e1f6b594
Nandkishor J <snape0407@gmail.com>
2021-09-26 20:52:13 +0530

    Nested if statements

--- main.cpp
+++ main.cpp
@@ -8,10 +8,33 @@ int main()
     double c;
     cout << "Enter a:\n";
     cin >> a;
-    cout << "Enter b\n";
+    cout << "Enter b:\n";
     cin >> b;
-    cout << "Enter c\n";
+    cout << "Enter Operator:1 for addition, 2 for subtraction, 3 for multiplication, 4 for division\n";
     cin >> c;
-   
+
+    if (c == 1)
+    {
+        cout << "A+B=" << a + b << "\n";
+    }
+    if (c == 2)
+    {
+        cout << "A-B=" << a - b << "\n";
+    }
+    if (c == 3)
+    {
+        cout << "A*B=" << a * b << endl;
+    }
+    if (c == 4)
+    {
+        if (b != 0)
+        {
+            cout << "A/B=" << a / b << endl;
+        }
+        else
+        {
+            cout << "Error due to division by 0\n";
+        }
+    }
     return 0;
 }
