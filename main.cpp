43bd1765becf4bb8e30d63d17c116cd1af066954
Nandkishor J <snape0407@gmail.com>
2021-10-05 18:29:14 +0530

    String length

--- main.cpp
+++ main.cpp
@@ -4,21 +4,16 @@ using namespace std;
 
 int main()
 {
-    string name;
-    int age;
-    cout << "Enter your fullname\n";
-    getline(cin, name);
-    cout << "Enter your age\n";
-    cin >> age;
-    cout << "Your name is: " << name << "\n";
-    cout << "Your age is: " << age << endl;
-    if(age >= 60)
-    {
-        cout << "You are a senior citizen\n";
-    }
-    else
-    {
-        cout << "You got a long life ahead\n";
-    }
+    string a;
+    string b;
+    string c;
+    cout << "Enter first line" << endl;
+    getline(cin, a);
+    cout << "Enter second line\n";
+    getline(cin, b);
+    cout << "Enter third line\n";
+    getline(cin, c);
+    double len = (a.length() + b.length() + c.length()) / 3.0;
+    cout << "Average length = " << len;
     return 0;
 }
