678587408259f588cd6aeecde73d1576a11385dd
Nandkishor J <snape0407@gmail.com>
2022-02-04 21:21:56 +0530

    strings

--- test.cpp
+++ test.cpp
@@ -1,13 +1,19 @@
 #include <iostream>
-#include <cmath>
+#include <string>
 using namespace std;
 
 int main()
 {
-    double x;
-    cout << "Enter x: \n";
-    cin >> x;
-    cout << "Cosh of x= " << cosh(x) << endl;
-    cout << "Another result of x: " << 1.0 / 2.0 * (exp(x) + exp(-x)) << "\n";
+    string firstname;
+    string lastname;
+    string age;
+    string a= ",your boomer";
+    cout << "Enter your first name: \n";
+    cin >> firstname;
+    cout << "Enter your last name: \n";
+    cin >> lastname;
+    cout << "Enter your age: \n";
+    cin >> age;
+    cout << "Your name is " << firstname.append(" ").append(lastname).append(" ") << "and your age is " << age.append(a);
     return 0;
-}
+} 
